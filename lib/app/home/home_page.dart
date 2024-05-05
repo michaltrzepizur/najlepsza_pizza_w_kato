eb7250226353ae3db43ba0569ce758d6f32f6f28
Michał Trzepizur <ramzes21246@gmail.com>
2024-05-05 18:27:04 +0200

    Implement sign out

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -13,7 +13,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Jesteś zalogowany jako ${user.email}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Jesteś zalogowany jako ${user.email}'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 240, 110, 121),
+              ),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: const Text('Wyloguj'),
+            ),
+          ],
+        ),
       ),
     );
   }
