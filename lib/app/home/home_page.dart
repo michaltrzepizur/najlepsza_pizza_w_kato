3489790af0b10ab2f440a5a6846f5bdf0549adab
Michał Trzepizur <ramzes21246@gmail.com>
2024-05-06 21:28:21 +0200

    Add opinion data

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -20,35 +21,18 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     if (currentIndex == 0) {}
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 156, 232, 228),
+        title: const Text('Najlepsza Pizza w Kato'),
+      ),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return Center(
-            child: Text('Jeden'),
-          );
+          return const RestaurantsPageContent();
         }
         if (currentIndex == 1) {
-          return Center(
-            child: Text('Dwa'),
-          );
+          return const AddOpinionPageContent();
         }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Jesteś zalogowany jako ${widget.user.email}'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 245, 171, 170),
-                ),
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('Wyloguj'),
-              ),
-            ],
-          ),
-        );
+        return MyAccountPageContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
@@ -73,5 +57,96 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+}
+
+class MyAccountPageContent extends StatelessWidget {
+  const MyAccountPageContent({
+    super.key,
+    required this.email,
+  });
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Jesteś zalogowany jako $email'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 245, 171, 170),
+            ),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Text('Wyloguj'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddOpinionPageContent extends StatelessWidget {
+  const AddOpinionPageContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Dwa'),
+    );
+  }
+}
+
+class RestaurantsPageContent extends StatelessWidget {
+  const RestaurantsPageContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream:
+            FirebaseFirestore.instance.collection('restaurants').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('coś poszło nie tak...'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text("Wczytywanie..."));
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(document['name']),
+                          Text(document['pizza']),
+                        ],
+                      ),
+                      Text(document['rating'].toString()),
+                    ],
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
   }
 }
