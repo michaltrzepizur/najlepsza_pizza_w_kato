34edabf2a6ad7a0bc6e78a2b6ac97a3f1442ef17
Michał Trzepizur <ramzes21246@gmail.com>
2024-05-07 18:35:43 +0200

    Clean code pages

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:najlepsza_pizza_w_kato/app/home/add_opinion/add_opinion_page_content.dart';
+import 'package:najlepsza_pizza_w_kato/app/home/my_account/my_account_page_content.dart';
+import 'package:najlepsza_pizza_w_kato/app/home/restaurants/restaurants_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -57,96 +59,5 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
-  }
-}
-
-class MyAccountPageContent extends StatelessWidget {
-  const MyAccountPageContent({
-    super.key,
-    required this.email,
-  });
-
-  final String? email;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Jesteś zalogowany jako $email'),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 245, 171, 170),
-            ),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text('Wyloguj'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AddOpinionPageContent extends StatelessWidget {
-  const AddOpinionPageContent({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
-    );
-  }
-}
-
-class RestaurantsPageContent extends StatelessWidget {
-  const RestaurantsPageContent({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream:
-            FirebaseFirestore.instance.collection('restaurants').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('coś poszło nie tak...'));
-          }
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text("Wczytywanie..."));
-          }
-
-          final documents = snapshot.data!.docs;
-
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(document['name']),
-                          Text(document['pizza']),
-                        ],
-                      ),
-                      Text(document['rating'].toString()),
-                    ],
-                  ),
-                ),
-              ],
-            ],
-          );
-        });
   }
 }
---
+++ lib/app/home/my_account/my_account_page_content.dart
@@ -0,0 +1,34 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyAccountPageContent extends StatelessWidget {
+  const MyAccountPageContent({
+    super.key,
+    required this.email,
+  });
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Jesteś zalogowany jako $email'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 245, 171, 170),
+            ),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Text('Wyloguj'),
+          ),
+        ],
+      ),
+    );
+  }
+}
